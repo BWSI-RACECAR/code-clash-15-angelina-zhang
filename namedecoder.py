68ec9e6f7345d3a38592fc8c46f20152ada2ce15
Angelina Zhang <85713648+angelina-zhang@users.noreply.github.com>
2022-07-29 07:26:34 -0700

    Add files via upload

---
+++ namedecoder.py
@@ -0,0 +1,105 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2022
+
+Code Clash #15 - Name Decoder (namedecoder.py)
+
+
+Author: Carter Berlind
+
+Difficulty Level: 5/10
+
+Prompt: Gregory asks you to help him write a program to decode some users of a program he built. 
+You need to write a function that accepts an encoded string as a parameter. This string will contain 
+a first name, last name, and an id.
+
+Values in the string can be separated by any number of zeros. The id is a numeric value but will 
+contain no zeros. The function should parse the string and return a string that contains the first 
+name, last name, and id values. (Note that the shown examples are all people who have had major 
+influences on the field of robotics, if there is someone you do not know please look them up to 
+learn some of the history and figures of this amazing field!)
+
+Constraints: Strings have no spaces in them and only contain first names, last names, and numeric 
+values with zeros to separate them. There does not need to be a zero separating the last name from the id. 
+
+Test Cases:
+Input: “Isaac00Asimov0001”          ; Output: “First name = Isaac, Last name = Asimov, id = 1”
+Input: “Joeseph0000Engleberger215”  ; Output: “First name = Joseph, Last name = Engleberger, id = 215”
+Input: “Raffaello0DAndea00003178”   ; Output: “First name = Raffaello, Last name = DAndea, id = 3178”
+Input: “Marc00000000Raibert0023”    ; Output: “First name = Marc, Last name = Raibert, id = 23”
+Input: “Daniela00000Rus0132”        ; Output: “First name = Daniela, Last name = Rus, id = 132”
+"""
+
+from re import I
+
+
+class Solution:
+    def decoder(self,id):
+        # type id: string
+        # return: string
+
+        # TODO: Write code below to return a string with the solution to the prompt
+        # id = list(id)
+        # count = 0
+        # last = 0
+        # numb = 0
+        # sum = 1
+        # for i in range (0, len(id)):
+        #     if id[i] == 0:
+        #         count = i
+        #         break
+        # for z in range (count, len(id)):
+        #     if id[z] != 0:
+        #         last = z
+        #         break
+        # for p in range (last, len(id)):
+        #     if id[p] == 0:
+        #         numb = p
+        #         break
+        # for z in range (numb, len(id)):
+        #     if id[z] != 0:
+        #         sum = z
+        #         break
+        # first = id[0:count]
+        # second = id[last:numb]
+        # third = id[sum:]
+        # id = list(id)
+        first = ""
+        last = ""
+        numb = ""
+        mode = 1
+        for i in range (0, len(id)):
+            if id[i] != "0":
+                if mode == 1:
+                    first += id[i]
+                elif mode == 2:
+                    if id[i].isnumeric():
+                        mode = 3
+                        third += id[i]
+                    else:
+                        last += id[i]
+                else:
+                    numb += id[i]
+            else:
+                if id[i-1]!= "0":
+                    mode+=1
+                else:
+                    pass
+              
+            
+
+        final = "First name = {0}, Last name =  {1}, id = {2}".format(first, last, numb)
+        return final
+
+
+
+def main():
+    string1 = input()
+
+    tc1 = Solution()
+    ans = tc1.decoder(string1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
